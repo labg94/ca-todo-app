30d962f1075168a08ff0b46b1a9104b1db20c5a1
Luis Benavides <luisalejobg@gmail.com>
2022-05-31 19:29:30 -0400

    ✅testing in Button

---
+++ src/presentation/todo/atoms/Button.test.tsx
@@ -0,0 +1,29 @@
+import {shallow} from "enzyme"
+import {Button} from "./Button";
+import {findByTestAttr} from "../../../../test/testUtils";
+
+describe("Button Test", () => {
+
+    describe("when the button is rendered", () => {
+
+        const text = "Dummy text"
+        const onClick = jest.fn()
+
+        const wrapper = shallow(<Button onClick={onClick}>{text}</Button>);
+        const buttonTag = findByTestAttr(wrapper, "button");
+
+
+        test("the text should be the same as the props", () => {
+            expect(buttonTag.text()).toBe(text)
+        });
+
+        test("the onClick should be the same as the props", () => {
+            buttonTag.simulate("click")
+            expect(onClick).toHaveBeenCalledTimes(1)
+        })
+
+
+    })
+
+
+})
--- src/presentation/todo/atoms/Button.tsx
+++ src/presentation/todo/atoms/Button.tsx
@@ -1,4 +1,5 @@
 import React from "react";
 
 export const Button = ({onClick, children}: { onClick: () => void, children: string }) => <button
+    data-test={"button"}
     onClick={onClick}>{children}</button>;
