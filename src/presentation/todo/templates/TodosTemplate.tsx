3deaeec770e33995c6180a483de9fa957d3e7d78
Luis Benavides <luisalejobg@gmail.com>
2022-05-31 11:31:47 -0400

    ♻️ moving header as atom

--- src/presentation/todo/templates/TodosTemplate.tsx
+++ src/presentation/todo/templates/TodosTemplate.tsx
@@ -3,6 +3,7 @@ import {TodosList} from "../organisms/TodosList";
 import {TextInput} from "../atoms/TextInput";
 import {Button} from "../atoms/Button";
 import React from "react";
+import {Header1} from "../atoms/Header1";
 
 export interface TodosTemplateProps {
     todos: Todos,
@@ -13,15 +14,9 @@ export interface TodosTemplateProps {
     header: string
 }
 
-function Header1({children}: { children: string }) {
-    return <h1>{children}</h1>;
-}
-
-export function TodosTemplate({todos, updateTodos, task, updateTask, createTask, header}: TodosTemplateProps) {
-    return <div>
-        <Header1>{header}</Header1>
-        <TodosList todos={todos} updateTodos={updateTodos}/>
-        <TextInput value={task} onChange={updateTask}/>
-        <Button onClick={createTask}>Create</Button>
-    </div>;
-}
+export const TodosTemplate = ({todos, updateTodos, task, updateTask, createTask, header}: TodosTemplateProps) => <div>
+    <Header1>{header}</Header1>
+    <TodosList todos={todos} updateTodos={updateTodos}/>
+    <TextInput value={task} onChange={updateTask}/>
+    <Button onClick={createTask}>Create</Button>
+</div>;
