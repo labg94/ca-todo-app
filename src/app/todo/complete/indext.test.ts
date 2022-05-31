095a9fb99e86a6328e25309ddf64c1cd961fe4eb
Luis Benavides <luisalejobg@gmail.com>
2022-05-31 18:23:11 -0400

    ✅testing the complete application

---
+++ src/app/todo/complete/indext.test.ts
@@ -0,0 +1,10 @@
+import {completeTodo} from "./index";
+import {CompleteTodoImpl} from "./CompleteTodoImpl";
+
+
+describe("CompleteTodo test", () => {
+
+    test("the instance should be the type CompleteTodoImpl", () =>
+        expect(completeTodo instanceof CompleteTodoImpl).toBeTruthy());
+
+})
