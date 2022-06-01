2d3f34e5c072f30a7c80e2842b18fabb34e86ae5
Luis Benavides <luisalejobg@gmail.com>
2022-06-01 11:23:57 -0400

    ✅testing TextInput

--- src/presentation/todo/atoms/TextInput.tsx
+++ src/presentation/todo/atoms/TextInput.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export const TextInput = ({value, onChange}: { value: string, onChange: (taskInput: string) => void }) => <input
+    data-test={"text-input"}
     type="text"
     value={value}
     onChange={({target: {value}}) => {
