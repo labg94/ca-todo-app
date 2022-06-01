48bb594a1db2a70a720b154790518d53e43b2077
Luis Benavides <luisalejobg@gmail.com>
2022-06-01 11:08:00 -0400

    ✅testing Loader

---
+++ src/presentation/todo/atoms/Loader.test.tsx
@@ -0,0 +1,28 @@
+import {shallow} from "enzyme";
+import {Loader} from "./Loader";
+import {findByTestAttr} from "../../../../test/testUtils";
+
+
+describe("Loader test", () => {
+
+
+    describe("when rendered", () => {
+
+        const wrapper = shallow(<Loader/>);
+
+        const loaderTag = findByTestAttr(wrapper, "loader");
+
+
+        test("The loader should be displayed", () => {
+            expect(loaderTag.exists()).toBeTruthy()
+        });
+
+        test("the text of the loader should be", () => {
+            expect(loaderTag.text()).toBe("Loading todos...")
+        });
+
+
+    })
+
+
+})
--- src/presentation/todo/atoms/Loader.tsx
+++ src/presentation/todo/atoms/Loader.tsx
@@ -1,3 +1,3 @@
 import React from "react";
 
-export const Loader = () => <div>Loading todos...</div>;
+export const Loader = () => <div data-test={"loader"}>Loading todos...</div>;
